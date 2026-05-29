29c1abf59ffaf03f4b149d040ee806b13171ea8a
d11416214-dotcom <d11416214@gs.takming.edu.tw>
2026-05-29 12:08:12 +0800

    Add files via upload

---
+++ D11416214_W11-4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace D11416214_W11_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(85, 96);
+            Student s2 = new Student(60,75);
+
+            Student savg=s1+s2;
+            Console.WriteLine(savg.Mid);
+            Console.WriteLine(savg.Final);
+        }
+    }
+}
